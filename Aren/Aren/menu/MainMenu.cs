33213bd1c97493767b4b3e24a746c98250277323
rakosmanjr <rakosmanjr@e319105b-631c-4da9-acfd-e3bcbc882c52>
2012-03-07 23:06:33 +0000

    Removed text from MainMenu.png Some more changes in MainMenu.cs Removed engine/menus (Rewriting it outside of the engine folder)

--- Aren/Aren/menu/MainMenu.cs
+++ Aren/Aren/menu/MainMenu.cs
@@ -3,81 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PloobsEngine.SceneControl;
+using PloobsEngine.Physic2D.Farseer;
 using PloobsEngine.Physics;
 using Microsoft.Xna.Framework;
-using Aren.engine.settings;
-using Microsoft.Xna.Framework.Graphics;
+using PloobsEngine.SceneControl._2DScene;
+using PloobsEngine.Particles;
 
 namespace Aren.menu
 {
-	public class MainMenu : IScene
+	public class MainMenu : I2DScene
 	{
-		Texture2D mouseCursor;
-		SpriteFont font;
-
-		String[] menuButtonText;
-
-		protected override void SetWorldAndRenderTechnich (out IRenderTechnic renderTech, out IWorld world)
+		protected override void SetWorldAndRenderTechnich (out RenderTechnich2D renderTech, out I2DWorld world)
 		{
-			BepuPhysicWorld bpw = new BepuPhysicWorld (-9.8F, true, 1.0F);
-			
-			world = new IWorld (bpw, new SimpleCuller ());
-
-			DeferredRenderTechnicInitDescription desc = DeferredRenderTechnicInitDescription.Default ();
-			desc.UseFloatingBufferForLightMap = true;
-			renderTech = new DeferredRenderTechnic (desc);
+			world = new I2DWorld(new FarseerWorld(new Vector2(0, -1)));
+			renderTech = new Basic2DRenderTechnich();
 		}
 
 		protected override void InitScreen (PloobsEngine.Engine.GraphicInfo GraphicInfo, PloobsEngine.Engine.EngineStuff engine)
 		{
-			base.InitScreen (GraphicInfo, engine);
+			base.InitScreen(GraphicInfo, engine);
 		}
 
 		protected override void LoadContent (PloobsEngine.Engine.GraphicInfo GraphicInfo, PloobsEngine.Engine.GraphicFactory factory, IContentManager contentManager)
 		{
-			base.LoadContent (GraphicInfo, factory, contentManager);
-
-			//
-			//Setup other graphics
-			//
-			mouseCursor = factory.GetTexture2D ("Images//mouse//defualt");
-		 	font = factory.GetSpriteFont ("MenuFont");
-			
-			//
-			//Intialize text array
-			//
-			menuButtonText = new String[3];
-		}
-
-		protected override void AfterLoadContent (IContentManager manager, PloobsEngine.Engine.GraphicInfo ginfo, PloobsEngine.Engine.GraphicFactory factory)
-		{
-			base.AfterLoadContent (manager, ginfo, factory);
+			base.LoadContent(GraphicInfo, factory, contentManager);
 		}
 
 		protected override void Update (Microsoft.Xna.Framework.GameTime gameTime)
 		{
-			base.Update (gameTime);
+			base.Update(gameTime);
 		}
 
 		protected override void Draw (Microsoft.Xna.Framework.GameTime gameTime, RenderHelper render)
 		{
-			base.Draw (gameTime, render);
-
-			//Draw text
-			render.RenderTextComplete ("This is the main menu!", new Vector2 (20, 20), Color.White, Matrix.Identity);
-
-			for (int i = 0; i < (menuButtonText.GetLength (0) + 1); i++)
-			{
-				render.RenderTextComplete (menuButtonText[i],
-					new Vector2 (100, (20 * i)), Color.White,
-					Matrix.Identity, font);
-			}
-
-			//Draw Mouse
-			if (EngineSettings.freeMouse)
-			{
-				render.RenderTextureComplete(mouseCursor, Color.White, new Rectangle(InputStates.mstate.X, InputStates.mstate.Y, 32, 32), Matrix.Identity);
-			}
+			base.Draw(gameTime, render);
 		}
 	}
 }
