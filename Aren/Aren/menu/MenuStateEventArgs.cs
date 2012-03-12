1ee4fb58b46e898eb807191e04451f55d1580849
rakosmanjr <rakosmanjr@e319105b-631c-4da9-acfd-e3bcbc882c52>
2012-03-12 22:30:16 +0000

    game starts when you press play, and exits when you press exit
    there is an enum that represents what state the menu is in: 'MenuStateEventArgs.states'

---
+++ Aren/Aren/menu/MenuStateEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aren.menu
+{
+	class MenuStateEventArgs : EventArgs
+	{
+		public enum states
+		{
+			main = 1,
+			play,
+			cont,
+			load,
+			save,
+			options,
+			exit
+		}
+
+		public states state;
+
+		public MenuStateEventArgs (states changeTo)
+		{
+			state = changeTo;
+		}
+	}
+}
